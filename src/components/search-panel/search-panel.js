286cfeab7be618ab4a8654af6c0f2c0d3865115b
Max <efamagistr@gmail.com>
2021-04-09 22:36:59 +0300

    “start”

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchPanel = () => {
+    return (
+        <input
+            className="form-control search-input"
+            type="type"
+            placeholder="Поиск по записям"
+        />
+    )
+}
+
+export default SearchPanel;
