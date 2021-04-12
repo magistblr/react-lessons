6a67d151db7745cd81ad133566f64bc664918d80
Max <efamagistr@gmail.com>
2021-04-13 00:46:31 +0300

    finish

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import './app-header.css'
 
-const AppHeader = () => {
+const AppHeader = ({liked, allPosts}) => {
     return (
         <div className="app-header d-flex">
-            <h1>Carl Max</h1>
-            <h2>5 записей, из них понравилось 0</h2>
+            <h1>Chef Max</h1>
+            <h2>{allPosts} записей, из них понравилось {liked}</h2>
         </div>
     )
 }
